3c0c435d092dd168c9e1bc7618a6275d548aec79
Junsung Choi <choiexe1@gmail.com>
2025-07-12 21:50:52 +0900

    router 변경

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,13 +1,18 @@
 import 'package:get/get.dart';
 import 'package:get_study/config/routes/app_routes.dart';
+import 'package:get_study/features/auth/bindings/login_bindings.dart';
+import 'package:get_study/features/auth/presentation/pages/login_page.dart';
 import 'package:get_study/features/home/bindings/home_bindings.dart';
 import 'package:get_study/features/home/presentation/pages/home_page.dart';
 import 'package:get_study/features/i18n/bindings/i18n_bindings.dart';
-import 'package:get_study/features/login/presentation/pages/login_page.dart';
 
 class AppRouter {
   static final routes = [
-    GetPage(name: Routes.login, page: () => const LoginPage()),
+    GetPage(
+      name: Routes.login,
+      page: () => const LoginPage(),
+      binding: LoginBindings(),
+    ),
     GetPage(
       name: Routes.home,
       page: () => const HomePage(),
