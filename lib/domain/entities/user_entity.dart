c3a6ddf7d8899e72d2726fd81cd4a011744dd69e
Junsung Choi <choiexe1@gmail.com>
2025-07-13 14:13:22 +0900

    fromJson toJson

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -14,6 +14,19 @@ class UserEntity {
   final String password;
   final String name;
 
+  factory UserEntity.fromJson(Map<String, dynamic> json) {
+    return UserEntity(
+      id: json['id'] as int,
+      username: json['username'] as String,
+      password: json['password'] as String,
+      name: json['name'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'username': username, 'password': password, 'name': name};
+  }
+
   @override
   String toString() {
     return 'UserEntity(username: $username, password: $password, name: $name)';
