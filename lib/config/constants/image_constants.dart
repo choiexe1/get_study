86c9f1c906d9edc2167a73a9226dbc4cdf7c9d03
Junsung Choi <choiexe1@gmail.com>
2025-07-13 14:13:27 +0900

    logo images

--- lib/config/constants/image_constants.dart
+++ lib/config/constants/image_constants.dart
@@ -1,3 +1,6 @@
 abstract class ImageConstants {
   static const String bg1 = 'assets/images/bg-1.jpg';
+  static const String bitcoinLogo = 'assets/images/bitcoin_logo.svg';
+  static const String ethereumLogo = 'assets/images/ethereum_logo.svg';
+  static const String tronTrxLogo = 'assets/images/tron_trx_logo.svg';
 }
