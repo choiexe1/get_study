c789c8664cc8ac9a50369339a3221201b67c1e66
Junsung Choi <choiexe1@gmail.com>
2025-07-13 14:13:37 +0900

    현재 유저를 가져오는 키

--- lib/config/constants/storage_key.dart
+++ lib/config/constants/storage_key.dart
@@ -1,4 +1,5 @@
 abstract class StorageKey {
   static const String username = 'username';
+  static const String currentUser = 'currentUser';
   static const String saveUsername = 'saveUsername';
 }
